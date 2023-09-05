8d06a49d8120cb07f50dac97955a9567cf61c8e1
fluffymariam <adefowobiladejobi@gmail.com>
2023-09-05 16:55:37 -0700

    k

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
-ascii_value = ord('a')
-
-while ascii_value <= ord('z'):
-    print(chr(ascii_value), end="")
-    ascii_value += 1
-
-print()
+for letters in range(97, 123):
+    print("{}".format(chr(letters)), end="")
