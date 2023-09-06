edde3b0ddc9f3e6e93077f9d2b9d07abb9b9094d
fluffymariam <adefowobiladejobi@gmail.com>
2023-09-05 17:10:58 -0700

    o

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,4 @@
 
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        if ones_digit == 8 and tens_digit == 9:
-            print("{}{}".format(ones_digit, tens_digit))
-        else:
-            print("{}{}".format(ones_digit, tens_digit), end=", ")
+        print("{:02}, ".format(10 * tens_digit + ones_digit), end="")
