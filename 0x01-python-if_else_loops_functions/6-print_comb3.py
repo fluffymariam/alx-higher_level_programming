c6ad567b2f100c27a086b3848b1921a029056c1a
fluffymariam <adefowobiladejobi@gmail.com>
2023-09-05 17:46:16 -0700

    l

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{:02}, ".format(10 * tens_digit + ones_digit), end="")
+for d1 in range(0, 10):
+    for d2 in range(d1 + 1, 10):
+        if d1 == 8 and d2 == 9:
+            print("{}{}".format(d1, d2))
+        else:
+            print("{}{}".format(d1, d2), end=", ")
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+def print_last_digit(numbers):
+    print(abs(numbers) % 10, end="")
+    return (abs(numbers) % 10)
