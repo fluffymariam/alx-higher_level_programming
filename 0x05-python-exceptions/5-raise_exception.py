431ed522dfe8792b3fe9f9ad64c8c169fe7d3793
fluffymariam <adefowobiladejobi@gmail.com>
2023-09-25 13:01:33 -0700

    p

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -2,10 +2,3 @@
 
 def raise_exception():
     raise TypeError("Exception raised")
-
-# Example usage
-if __name__ == "__main__":
-    try:
-        raise_exception()
-    except TypeError as te:
-        print("Exception raised")
