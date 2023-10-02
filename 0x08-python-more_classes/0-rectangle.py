11b7426340fd880c3aa15fca9c205dbc8368c4de
fluffymariam <adefowobiladejobi@gmail.com>
2023-10-02 11:53:06 -0700

    8

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defining a Rectangle class."""
+
+class Rectangle:
+    """This is a simple rectangle class."""
+    pass
+
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+s is my project on more classes in python
