d2572a82a693997dafa724d615ca37a0b6072369
fluffymariam <adefowobiladejobi@gmail.com>
2023-10-02 10:52:02 -0700

    l

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+"""Defines an integer addition function."""
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+
+    Args:
+        a (int): The first integer.
+        b (int, optional): The second integer. Defaults to 98.
+
+    Returns:
+        int: The addition of a and b.
+
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
