cc7c84ac609e74785ce8def8011df98dbb98bf5d
fluffymariam <adefowobiladejobi@gmail.com>
2023-10-10 14:00:37 -0700

    pp

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,15 +8,18 @@ import sys
 import json
 from os.path import exists
 
+
 def save_to_json_file(my_list, filename):
     with open(filename, 'w') as json_file:
         json.dump(my_list, json_file)
+
 
 def load_from_json_file(filename):
     if exists(filename):
         with open(filename, 'r') as json_file:
             return json.load(json_file)
     return []
+
 
 if __name__ == "__main__":
     filename = "add_item.json"
