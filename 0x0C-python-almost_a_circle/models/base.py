b82db1fa6e0c644c7ffb728286c79005b9f01ea7
fluffymariam <adefowobiladejobi@gmail.com>
2023-10-16 15:05:23 -0700

    leap

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,32 +1,92 @@
 #!/usr/bin/python3
 """Defines a Base class."""
 
+import json
+import turtle
+
 
 class Base:
     """Represents the base class."""
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initializes a new instance of the Base class."""
+        """Initializes a Base instance."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
-if __name__ == "__main__":
-    b1 = Base()
-    print(b1.id)
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string representation of list_objs to a file."""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as file:
+            if list_objs is None:
+                file.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                file.write(cls.to_json_string(list_dicts))
 
-    b2 = Base()
-    print(b2.id)
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list represented by json_string."""
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
 
-    b3 = Base()
-    print(b3.id)
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)  # Create a dummy Rectangle instance
+        elif cls.__name__ == "Square":
+            dummy = cls(1)  # Create a dummy Square instance
+        dummy.update(**dictionary)  # Upda with real values from dictionary
+        return dummy
 
-    b4 = Base(12)
-    print(b4.id)
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances."""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                json_string = file.read()
+                list_dicts = cls.from_json_string(json_string)
+                return [cls.create(**d) for d in list_dicts]
+        except FileNotFoundError:
+            return []
 
-    b5 = Base()
-    print(b5.id)
+    @staticmethod
+    def draw(list_rectangles, list_squares)
+    """Draws all ththe Turtle graphics module."""
+        turtle.speed(0)
+        turtle.color("blue")
+        for rect in list_rectangles:
+            turtle.penup()
+            turtle.goto(rect.x, rect.y)
+            turtle.pendown()
+            for _ in range(2):
+                turtle.forward(rect.width)
+                turtle.left(90)
+                turtle.forward(rect.height)
+                turtle.left(90)
+
+        turtle.color("red")
+        for square in list_squares:
+            turtle.penup()
+            turtle.goto(square.x, square.y)
+            turtle.pendown()
+            for _ in range(4):
+                turtle.forward(square.size)
+                turtle.left(90)
+
+        turtle.done()
