7a66a700adba9a2a502096018fb220a33ce7144e
fluffymariam <adefowobiladejobi@gmail.com>
2023-10-16 14:40:17 -0700

    kill

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,7 +6,6 @@ class Base:
     """Represents the base class."""
     __nb_objects = 0
 
-
     def __init__(self, id=None):
         """Initializes a new instance of the Base class."""
         if id is not None:
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class that inherits from Base."""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Represents a rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes a Rectangle instance."""
+        super().__init__(id)  # Call the constructor of the Base class with id
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        """Getter method for width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter method for x."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter method for x."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter method for y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter method for y."""
+        self.__y = value
+
+
+if __name__ == "__main__":
+    r1 = Rectangle(10, 2)
+    print(r1.id)
+
+    r2 = Rectangle(2, 10)
+    print(r2.id)
+
+    r3 = Rectangle(10, 2, 0, 0, 12)
+    print(r3.id)
