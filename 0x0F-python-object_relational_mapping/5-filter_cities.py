6636d7eb5ebf739cf1546a7e5f24bbd924425d9d
fluffymariam <adefowobiladejobi@gmail.com>
2023-12-20 09:46:05 -0800

    ll

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
                 WHERE states.name=%s""", (sys.argv[4],))
     rows = cur.fetchall()
     tmp = list(row[0] for row in rows)
-        print(*tmp, sep=", ")
+    print(*tmp, sep=", ")
     cur.close()
     db.close()
